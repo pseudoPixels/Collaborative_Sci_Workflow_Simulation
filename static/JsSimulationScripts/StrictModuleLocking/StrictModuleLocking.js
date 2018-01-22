6816a142c329e664f169c233a2c3573bcba0567a
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-01-21 19:56:07 -0600

    More node access protocols for sim implemented.

--- static/JsSimulationScripts/StrictModuleLocking/StrictModuleLocking.js
+++ static/JsSimulationScripts/StrictModuleLocking/StrictModuleLocking.js
@@ -400,7 +400,7 @@ var workflow_instructions = [
     ['3528', 'addModule', '5857', 'updateParam', '7376', 'updateDatalink', '9151', 'updateDatalink', '9291', 'updateDatalink', '9617', 'addModule', '5950', 'updateDatalink', '9503', 'updateParam', '2768', 'addModule', '4276', 'addModule', '7776', 'addModule', '9616', 'addModule', '4532', 'addModule', '3541', 'updateDatalink', '9285', 'updateDatalink', '2935', 'updateParam', '9663', 'addModule', '9699', 'updateParam', '2121', 'updateParam', '5814', 'updateDatalink', '5407', 'updateParam', '4522', 'updateDatalink', '3295', 'addModule', '8920', 'updateParam', '5831', 'updateDatalink', '5174', 'updateParam', '6344', 'addModule', '9640', 'updateDatalink', '7478', 'addModule', '9116', 'updateDatalink', '5484', 'updateDatalink', '5487', 'updateDatalink', '2027', 'updateDatalink', '4013', 'addModule', '4768', 'addModule', '4506', 'updateParam', '5947', 'addModule', '2643', 'addModule', '4244', 'updateDatalink', '4681', 'updateParam', '3151', 'updateDatalink', '6798', 'addModule', '3108', 'addModule', '6339', 'updateParam', '8657', 'updateParam', '4193', 'addModule', '3862', 'addModule', '9561', 'updateDatalink', '5240', 'updateParam', '6372', 'updateDatalink', '9468', 'updateDatalink', '4582', 'updateDatalink', '9092', 'updateDatalink', '7616', 'updateParam', '5511', 'addModule', '3202', 'addModule', '8474', 'addModule', '8846', 'updateDatalink', '7344', 'updateParam', '7192', 'updateDatalink', '7376', 'addModule', '2532', 'updateParam', '4257', 'addModule', '9697', 'updateParam', '6171', 'updateDatalink', '6597', 'updateParam', '8622', 'addModule', '5644', 'addModule', '7274', 'updateDatalink', '3324', 'updateDatalink', '2057', 'updateDatalink', '3151', 'updateParam', '5793', 'updateDatalink', '7266', 'addModule', '2578', 'addModule', '3928', 'addModule', '5191', 'updateDatalink', '6143', 'updateParam', '4757', 'updateParam', '8686', 'addModule', '9939', 'updateParam', '6760', 'updateParam', '8880', 'addModule', '7547', 'addModule', '3173', 'updateDatalink', '9904', 'updateDatalink', '3096', 'addModule', '3442', 'addModule', '5351', 'updateParam', '2253', 'updateDatalink', '2584', 'updateParam', '4524', 'addModule', '5908', 'addModule', '5286', 'addModule', '9701', 'addModule', '9571', 'updateParam', '7913', 'addModule', '5184', 'updateDatalink', '7141', 'updateParam', '2393', 'updateParam']
 ];
 
-var INSTRUCTIONS_PER_COLLABORATOR = 5;
+var INSTRUCTIONS_PER_COLLABORATOR = 99;
 
 
 
@@ -514,6 +514,45 @@ WorkflowCollaborator.prototype.getNodeWithHigherDependencyDegree_exceptUserLocke
 
 
 
+//returns the node with lower Dependency degree that has not been user locked yet
+WorkflowCollaborator.prototype.getNodeWithLowerDependencyDegree_exceptUserLockedNode = function(){
+    var theNode = "n" + NUM_OF_MODULES.toString();//by default a leaf node, as it has the least dependency degree
+
+    for(var i=NUM_OF_MODULES;i>=1;i--){
+        if(isTheNodeInThisList(grantedNodeAccesses,"n"+i.toString()) == false){
+            theNode = "n"+i.toString();
+            break;
+        }
+    }
+
+    return theNode;
+
+};
+
+
+
+//returns the node with the lower dependency degree which has not been user locked or requested (waiting)
+//by any other collaborators
+WorkflowCollaborator.prototype.getNodeWithLowerDependencyDegree_exceptUserLockedAndWaitingNodes = function(){
+    var combinedList = grantedNodeAccesses.concat(waitingNodeAccessRequests);
+
+    var theNode = "n"+NUM_OF_MODULES.toString();//by default a leaf node, as it has the least dependency degree in case all node has been taken
+
+    //iterate from lower to higher dependency degree
+    for(var i=NUM_OF_MODULES;i>=1;i--){
+        if(isTheNodeInThisList(combinedList, "n"+i.toString()) == false){
+            theNode = "n"+i.toString();
+            break;
+        }
+    }
+
+    return theNode;
+
+};
+
+
+
+
 
 WorkflowCollaborator.prototype.simulate = function() {
     //do I have any instruction left to exec.
@@ -556,7 +595,7 @@ WorkflowCollaborator.prototype.simulate = function() {
         }else{//I dont have any access, so request for it
             if(this.isAccessRequestedAlready == false){//not requested yet..?, request access
                 this.isAccessRequestedAlready = true;
-                console.log("NODE_ACCESS_REQUESTED"+ "_" + this.collaboratorID + " (NodeID: " + this.getNodeWithHigherDependencyDegree_exceptUserLockedAndWaitingNodes() + ")");
+                console.log("NODE_ACCESS_REQUESTED"+ "_" + this.collaboratorID + " (NodeID: " + this.getNodeWithLowerDependencyDegree_exceptUserLockedAndWaitingNodes() + ")");
 
                  //=====================
                  //Different protocols for node access request for the simulation
@@ -564,7 +603,9 @@ WorkflowCollaborator.prototype.simulate = function() {
                  //=====================
                 //newNodeAccessRequest(this.collaboratorID, "n1");//always requesting n1 (should behave as floor request), for testing
                 //newNodeAccessRequest(this.collaboratorID, this.getNodeWithHigherDependencyDegree_exceptUserLockedNode());//worst case, request always higher dependency degree
-                newNodeAccessRequest(this.collaboratorID, this.getNodeWithHigherDependencyDegree_exceptUserLockedAndWaitingNodes());
+                //newNodeAccessRequest(this.collaboratorID, this.getNodeWithHigherDependencyDegree_exceptUserLockedAndWaitingNodes());
+                newNodeAccessRequest(this.collaboratorID, this.getNodeWithLowerDependencyDegree_exceptUserLockedNode());
+                //newNodeAccessRequest(this.collaboratorID, this.getNodeWithLowerDependencyDegree_exceptUserLockedAndWaitingNodes());
 
                 var me = this;
                 setTimeout(function() {
